642867f4b907d52c6955381765187ed73cde0cb0
Arun Jose <njarunnj@gmail.com>
2023-09-28 00:42:55 +0400

    Initial Commit

---
+++ AppModule/src/main/java/com/dxb/truckmonitor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dxb.truckmonitor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
